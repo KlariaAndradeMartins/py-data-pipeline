7ddc6ddc8c8c8f99b22c844d0f0e9028b6508ef5
Klaria-data <141838855+Klaria-data@users.noreply.github.com>
2024-01-29 11:14:47 -0300

    Finish fusion.py

--- scripts/fusion_jan.py
+++ scripts/fusion_jan.py
@@ -41,22 +41,65 @@ def rename_columns(data_csv, key_mapping):
     
     return new_data_csv
 
+def size_data(data):
+    return len(data)
+
+def join_data(dataA, dataB):
+    combined_list = [] 
+    combined_list.extend(dataA)
+    combined_list.extend(dataB)
+
+    return combined_list
+
+def transform_data_table(data, columns_name):
+
+    data_combine_table = [columns_name]
+    for row in data:
+        line = []
+        for columns in columns_name:
+            line.append(row.get(columns, 'Indisponivel'))
+        data_combine_table.append(line)
+
+    return data_combine_table
+
+def saving_data(data, path):
+
+    with open(path, 'w') as file:
+        writer = csv.writer(file)
+        writer.writerows(data)
+
 path_json = 'data_raw/dados_empresaA.json'
 path_csv = 'data_raw/dados_empresaB.csv'
 
-#initializing reding the data
+#Extracting and reading data
 data_json = read_data(path_json, 'json')
 name_columns_json = get_columns(data=data_json)
-print(data_json[0], name_columns_json)
+size_data_json = size_data(data_json)
+print(f"Name columns json:{name_columns_json}, Size of json data: {size_data_json}\n")
 
 data_csv = read_data(path_csv, 'csv')
 name_columns_csv = get_columns(data=data_csv)
-print(data_csv[0], name_columns_csv)
+size_data_csv = size_data(data_csv)
+print(f"Name columns csv: {name_columns_csv}, Size of csv data: {size_data_csv}\n")
 
-#transform data
+#Transform data
 key_mapping = {'Nome do Item': 'Nome do Produto',
                 'Classificação do Produto': 'Categoria do Produto',
                 'Valor em Reais (R$)': 'Preço do Produto (R$)',
                 'Quantidade em Estoque': 'Quantidade em Estoque',
                 'Nome da Loja': 'Filial',
                 'Data da Venda': 'Data da Venda'}
+
+data_csv = rename_columns(data_csv, key_mapping)
+print(f"Rename columns name of csv: {get_columns(data_csv)}")
+
+data_fusion = join_data(data_csv, data_json)
+data_fusion_columns = get_columns(data_fusion)
+size_data_fusion = size_data(data_fusion)
+print(f"Name columns fusion:{data_fusion_columns}, Size of fuzion data:{size_data_fusion}")
+
+#Load data
+data_fusion_table = transform_data_table(data_fusion, data_fusion_columns)
+path_data_combined = "data_processed/data_combined.csv"
+
+saving_data(data_fusion_table, path=path_data_combined)
